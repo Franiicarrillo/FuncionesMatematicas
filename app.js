bbd9c5d26264959049c55cf3f5772f64943cf947
Francisca <Franiicarrillo@gmail.com>
2018-05-27 17:56:36 -0400

    Termine el primemr ejercicio

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const comida =prompt("Que deseas comer")
-let food = function (comida){
-  if (DeseoComer = "ceviche") {
- alert("Estupenda eleccion")
-  }
+
+let add =function(numero1,numero2){
+
+  return numero1 +numero2
 }
+add(9824,777)
+
+let multiply = function(numero3,numero4){
+  return numero3 * numero4
+}
+ multiply(36325,add(9824,777));
+document.write(multiply(36325,add(9824,777))) ;
